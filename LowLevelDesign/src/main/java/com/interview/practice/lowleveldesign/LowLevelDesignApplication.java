cde5de53f88cd0102b8a90bad1ae030d7a5099b8
Rohan Patil <rohan_patil1@intuit.com>
2025-02-15 00:52:36 +0530

    created new module for LLD.

---
+++ LowLevelDesign/src/main/java/com/interview/practice/lowleveldesign/LowLevelDesignApplication.java
@@ -0,0 +1,13 @@
+package com.interview.practice.lowleveldesign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LowLevelDesignApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LowLevelDesignApplication.class, args);
+    }
+
+}
