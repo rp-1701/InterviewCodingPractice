c6f580c3162b762537fd9c3c568f2fef50c01c1e
Rohan Patil <rohan_patil1@intuit.com>
2025-02-23 00:49:31 +0530

    added new Module for Java core and Advance

---
+++ JavaCoreAndAdvance/src/main/java/com/interview/practice/javacoreandadvance/JavaCoreAndAdvanceApplication.java
@@ -0,0 +1,13 @@
+package com.interview.practice.javacoreandadvance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaCoreAndAdvanceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaCoreAndAdvanceApplication.class, args);
+    }
+
+}
