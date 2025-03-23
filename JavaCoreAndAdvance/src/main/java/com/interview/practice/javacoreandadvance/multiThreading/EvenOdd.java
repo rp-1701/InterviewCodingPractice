783b34f8c6fe71b4a26d8f522aed4e5c0baa2c51
Rohan Patil <rohan_patil1@intuit.com>
2025-03-23 20:14:47 +0530

    implemented inter-thread communication

---
+++ JavaCoreAndAdvance/src/main/java/com/interview/practice/javacoreandadvance/multiThreading/EvenOdd.java
@@ -0,0 +1,51 @@
+package com.interview.practice.javacoreandadvance.multiThreading;
+
+public class EvenOdd {
+
+    static boolean evenThreadTurn = false;
+    static final Object lock = new Object();
+
+    public static void main(String[] args) {
+
+        Thread evenThread = new Thread(() -> {
+
+            try {
+                for (int i = 2; i <= 20; i = i + 2) {
+                    synchronized (lock) {
+                        while (!evenThreadTurn) {
+                            lock.wait();
+                        }
+                        System.out.print(i + " ");
+                        evenThreadTurn = false;
+                        lock.notify();
+                    }
+                }
+            } catch (InterruptedException e) {
+                System.out.println("Error occurred in evenThread : " + e.getMessage());
+            }
+
+        });
+
+        Thread oddThread = new Thread(() -> {
+            try {
+                for (int i = 1; i <= 20; i = i + 2) {
+                    synchronized (lock) {
+                        while (evenThreadTurn) {
+                            lock.wait();
+                        }
+                        System.out.print(i + " ");
+                        evenThreadTurn = true;
+                        lock.notify();
+                    }
+                }
+            } catch (InterruptedException e) {
+                System.out.println("Error occurred in oddThread : " + e.getMessage());
+            }
+        });
+
+        oddThread.start();
+        evenThread.start();
+
+    }
+
+}
---
+++ JavaCoreAndAdvance/src/main/java/com/interview/practice/javacoreandadvance/multiThreading/LetterNumber.java
@@ -0,0 +1,49 @@
+package com.interview.practice.javacoreandadvance.multiThreading;
+
+public class LetterNumber {
+
+    private static final Object lock = new Object();
+    private static boolean letterThreadTurn = true;
+
+    public static void main(String[] args) {
+
+        Thread letterThread = new Thread(() -> {
+            try {
+                for (char c = 'A'; c <= 'Z'; c++) {
+                    synchronized (lock) {
+                        while(!letterThreadTurn) {
+                            lock.wait();
+                        }
+                        System.out.print(c + " ");
+                        letterThreadTurn = false;
+                        lock.notify();
+                    }
+
+                }
+            } catch (InterruptedException e) {
+                System.out.println("Error occurred in letterThread : " + e.getMessage());
+            }
+        });
+
+        Thread numberThread = new Thread(() -> {
+            try {
+                for (int i = 1; i <= 26; i++) {
+                    synchronized (lock) {
+                        while(letterThreadTurn) {
+                            lock.wait();
+                        }
+                        System.out.print(i + " ");
+                        letterThreadTurn = true;
+                        lock.notify();
+                    }
+                }
+            } catch (InterruptedException e) {
+                System.out.println("Error occurred in numberThread : " + e.getMessage());
+            }
+        });
+
+        letterThread.start();
+        numberThread.start();
+    }
+
+}
